5b6eb1466fee7f9bad2c24392000cc0d28a14af4
Onorio Catenacci <catenacci@ieee.org>
2023-04-17 11:46:11 -0400

    WIP

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -5,7 +5,7 @@
 # Inside the script, you can read and write to any of your
 # repositories directly:
 #
-#     Foodtruck.Repo.insert!(%Foodtruck.SomeSchema{})
+#     Streetfood.Repo.insert!(%Streetfood.SomeSchema{})
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
